89ad03ebe3391a5fe62c2f91768f29101821eab2
Rafał Laskowski <rafal.laskowski02@gmail.com>
2022-10-04 17:14:24 +0200

    Initial commit

---
+++ src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,13 @@
+package agh.ics.oop;
+
+public class World {
+    public static void run(){
+        System.out.println("im moving forward");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("system rozpoczol dzialanie");
+        run();
+        System.out.println("system zakonczyl dzialanie");
+    }
+}
